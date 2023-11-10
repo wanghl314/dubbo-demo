3e2e1dec2be69201f5865054a17e1cef9f75cd55
王洪亮 <w505957125@aliyun.com>
2023-11-10 10:55:36 +0800

    1. replace org.apache.commons.io.FileUtils with java nio api

--- dubbo-demo-server/src/main/java/com/whl/dubbo/FileServiceImpl.java
+++ dubbo-demo-server/src/main/java/com/whl/dubbo/FileServiceImpl.java
@@ -1,43 +1,48 @@
 package com.whl.dubbo;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.dubbo.common.utils.StringUtils;
 
 public class FileServiceImpl implements FileService {
 
     @Override
     public String upload(String name, InputStream is) throws IOException {
-        final String PATH = this.getFileStoreDirectory();
         String suffix = "";
 
         if (StringUtils.isContains(name, '.')) {
             suffix = name.substring(name.lastIndexOf("."));
         }
         String storeFileName = UUID.randomUUID() + suffix;
-        File destination = new File(PATH + File.separator + storeFileName);
-        FileUtils.copyInputStreamToFile(is, destination);
+        Path path = this.getFileStorePath();
+        Path storePath = path.resolve(storeFileName);
+
+        if (!Files.exists(storePath.getParent())) {
+            Files.createDirectories(storePath.getParent());
+        }
+        Files.copy(is, storePath, StandardCopyOption.REPLACE_EXISTING);
         return storeFileName;
     }
 
     @Override
     public InputStream download(String name) throws IOException {
-        final String PATH = this.getFileStoreDirectory();
-        File file = new File(PATH + File.separator + name);
+        Path path = this.getFileStorePath();
+        Path file = path.resolve(name);
 
-        if (file.exists()) {
-            return Files.newInputStream(file.toPath());
+        if (Files.exists(file)) {
+            return Files.newInputStream(file);
         }
         return null;
     }
 
-    private String getFileStoreDirectory() {
-        return System.getProperty("user.dir") + File.separator + "upload";
+    private Path getFileStorePath() {
+        return Paths.get(System.getProperty("user.dir"), "upload");
     }
 
 }
